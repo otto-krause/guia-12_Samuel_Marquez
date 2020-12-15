da9e986ce1912fdebd041ca8d39e94e12b71d517
gusgatam <gustavo.gatamora@gmail.com>
2020-12-15 20:26:28 -0300

    Devolución.
    Cometarios en los ejercicios.

--- Guia-12_Samuel_Marquez/Ej2/main.c
+++ Guia-12_Samuel_Marquez/Ej2/main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+//Idem ejercicio anterior.
+int area(int aa, int bb);
+int perimetro(int aa, int bb);
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+	/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+	int main()
+{
+	int aa = 0, bb = 0, resul = 0, res1, res2;
+	printf(" Ingrese un numero ");
+	scanf("%d", &aa);
+	printf(" Ingrese otro numero ");
+	scanf("%d", &bb);
+	res1 = area(aa, bb);
+	res2 = perimetro(aa, bb);
+	printf(" El area es %d", res1);
+	printf(" El perimetro es %d ", res2);
+}
 
-		int main( ) {
-			int aa=0,bb=0,resul=0,res1,res2;
-			printf(" Ingrese un numero ");
-			scanf("%d",&aa);
-			printf(" Ingrese otro numero ");		
-			scanf("%d",&bb);
-			res1=area(aa,bb);
-			res2=perimetro(aa,bb);
-			printf(" El area es %d",res1);
-			printf(" El perimetro es %d ",res2);
-					
-		}
-	
 	int area(int aa, int bb)
 	{
 		int ar=0;
